8decc927f2b877eb52207e81044b48611df95c4a
hadihamdan078 <hadihm078@gmail.com>
2026-03-05 23:27:18 +0300

    Create 2148A_Sublime Sequence.cpp

---
+++ 800/2148A_Sublime Sequence.cpp
@@ -0,0 +1,13 @@
+//https://codeforces.com/contest/2148/problem/A
+#include <iostream>
+using namespace std ;
+int main () {
+    int t;
+    cin>>t;
+    while (t--) {
+        int x,n;
+        cin>>x>>n;
+        if(n%2==0) cout<<0<<endl;
+        else cout<<x<<endl;
+    }
+    return 0 ;
