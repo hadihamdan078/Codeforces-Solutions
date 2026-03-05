b36c4d8730ce09c29049ee56c934b270705d73e3
hadihamdan078 <hadihm078@gmail.com>
2026-03-05 23:22:20 +0300

    Create 4A_watermelon.cpp

---
+++ 800/4A_watermelon.cpp
@@ -0,0 +1,13 @@
+//https://codeforces.com/contest/4/problem/A
+#include <iostream>
+using namespace std ;
+int main() {
+    int w;
+    cin>>w;
+    if(w>2 && w%2==0){
+    cout<<"YES";
+    }else{
+    cout<<"NO";
+    }
+    return 0;
+}
