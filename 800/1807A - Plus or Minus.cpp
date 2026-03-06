df9530fa267faa1f4c7694b3062c4ba739394e9b
hadihamdan078 <hadihm078@gmail.com>
2026-03-06 16:58:28 +0300

    Create 1807A - Plus or Minus.cpp

---
+++ 800/1807A - Plus or Minus.cpp
@@ -0,0 +1,13 @@
+//https://codeforces.com/problemset/problem/1807/A
+#include<iostream>
+using namespace std;
+int main(){
+  int t;
+  cin>>t;
+  while(t--){
+    int a , b , c;
+    cin>>a >> b >> c;
+    if(a+b==c) cout<<"+\n";
+    else cout<<"-\n";
+  }
+}
