c40d572680760b04ee0bafdd0113667ce7222c36
hadihamdan078 <hadihm078@gmail.com>
2026-03-09 08:57:51 +0300

    Create 2009A - Minimize!.cpp

---
+++ 800/2009A - Minimize!.cpp
@@ -0,0 +1,12 @@
+//https://codeforces.com/problemset/problem/2009/A
+#include<iostream>
+using namespace std;
+int main(){
+  int t;
+  cin>>t;
+  while(t--){
+    int a , b;
+    cin>>a  >> b;
+    cout<<b-a<<"\n";
+  }
+}
