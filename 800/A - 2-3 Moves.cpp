29fb36dd191619e2a64bfa31fee49ff719bdeb2a
hadihamdan078 <hadihm078@gmail.com>
2026-03-13 18:17:07 +0300

    Create A - 2-3 Moves.cpp

---
+++ 800/A - 2-3 Moves.cpp
@@ -0,0 +1,19 @@
+//https://codeforces.com/contest/1716/problem/A
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        if(n==1){
+            cout<<2<<"\n";
+        }else if(n%3==0){
+            cout<<n/3<<"\n";
+        }else{
+            cout<<n/3+1<<"\n";
+        }
+
+    }
+}
