22a426485f5d87a5eba68f716298ac2f38a52e55
hadi hamdan <hadihm078@gmail.com>
2026-03-19 09:16:44 +0300

    Create 1772A - A+B?.cpp

---
+++ 800/1772A - A+B?.cpp
@@ -0,0 +1,15 @@
+//https://codeforces.com/problemset/problem/1772/A
+#include<iostream>
+using namespace std;
+int main(){
+   ios_base::sync_with_stdio(false);
+   cin.tie(NULL);
+   int t;
+   cin>>t;
+   while(t--){
+    int a , b;
+    char plus;
+    cin>>a >> plus >> b;
+    cout<<a+b<<"\n";
+   }
+}
