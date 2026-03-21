364e7edc4e8ddaeff370c31377e04c61f2ce04f8
hadi hamdan <hadihm078@gmail.com>
2026-03-21 12:00:35 +0300

    Create 1537B - Bad-Boy.cpp

---
+++ 900/1537B - Bad-Boy.cpp
@@ -0,0 +1,14 @@
+//https://codeforces.com/problemset/problem/1537/B
+#include<iostream>
+using namespace std;
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--){
+      long long n , m , i , j;
+      cin>> n >> m >> i >> j ;
+       cout<<n<<" "<<1<<" "<<1<<" "<<m<<"\n";
+    }
+}
