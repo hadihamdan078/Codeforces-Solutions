a0361dea4ea54afb497f086bfa3f01fc18aa0d3c
hadi hamdan <hadihm078@gmail.com>
2026-03-27 20:49:36 +0300

    Create 	1339A - Filling-Diamonds.cpp

---
+++ 900/1339A - Filling-Diamonds.cpp
@@ -0,0 +1,14 @@
+//https://codeforces.com/problemset/problem/1339/A
+#include<iostream>
+using namespace std;
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL); 
+  int t;
+  cin>>t;
+  while(t--){
+    int n;
+    cin>>n;
+    cout<<n<<"\n";
+  }
+}
