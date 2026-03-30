cc8a8b04fb845b584dc68c1ccdaf7bf80709b8d3
hadi hamdan <hadihm078@gmail.com>
2026-03-30 23:27:38 +0300

    Create 1337A - Ichihime-and-Triangle.cpp

---
+++ 800/1337A - Ichihime-and-Triangle.cpp
@@ -0,0 +1,15 @@
+//https://codeforces.com/problemset/problem/1337/A
+#include<iostream>
+using namespace std;
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL); 
+  int t;
+  cin>>t;
+  while(t--){
+    long long a , b , c , d;
+    cin>> a >> b >> c >> d;
+    cout<<b<<" "<< c <<" "<< c<<"\n";
+  }
+  return 0;
+}
